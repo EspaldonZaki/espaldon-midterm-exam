19c069f97b5f6b43078e27aad4b655178229e6fa
EspaldonZaki <espaldonzl.359.stud@cdd.edu.ph>
2024-07-18 15:14:56 +0800

    Models

---
+++ app/Models/Post_comments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Post;
+use App\Models\Comment_replies;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post_comments extends Model
+{
+    use HasFactory;
+}
